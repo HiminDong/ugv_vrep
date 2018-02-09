1c1c00a9b20dbeed38caae56657a545ae176b031
h-utkuunlu <h.utkuunlu@gmail.com>
2018-02-08 19:08:54 -0500

    Scene edits & fixed tank drive to heading-velocity

--- ugv_main.py
+++ ugv_main.py
@@ -6,13 +6,12 @@ import numpy as np
 import cv2
 import pickle
 
-
-left = 0
-right = 0
+heading = 0
+velocity = 0
 
 def bound(val):
-    upperBound = 10
-    lowerBound = -10
+    upperBound = 1
+    lowerBound = -1
     if val >= upperBound:
         val = upperBound
     elif val <= lowerBound:
@@ -20,41 +19,38 @@ def bound(val):
     return val
 
 def on_press(key):
-    global left, right, handles
+    global velocity, heading, handles
     if key == Key.up: # Forward motion
-        left += 1
-        right += 1
+        velocity += 0.1
     elif key == Key.down: # Backward motion
-        left -= 1
-        right -= 1
+        velocity -= 0.1
     elif key == Key.left: # Backward motion
-        left -= 1
-        right += 1
+        heading += 0.08
     elif key == Key.right: # Backward motion
-        left += 1
-        right -= 1
+        heading -= 0.08
     elif key == Key.space:
-        left = 0
-        right = 0
-    left = bound(left)
-    right = bound(right)
-    print("Left= ", left, " - Right= ", right)
-    set_velocity(left*-1, right*-1, handles)
+        velocity = 0
+        heading = 0
+    heading = bound(heading)
+    velocity = bound(velocity)
+    print("Heading= ", heading, " - Velocity= ", velocity)
+    set_motion(heading, velocity, handles)
 
 def on_release(key):
     pass
 
-def set_velocity(left, right, handles):
+def set_motion(left, right, handles):
     global clientID
+
+    left = -10*(velocity - heading)
+    right =  -10*(velocity + heading)
+    
     vrep.simxSetJointTargetVelocity(clientID, handles[0], left, vrep.simx_opmode_streaming)
     vrep.simxSetJointTargetVelocity(clientID, handles[1], left, vrep.simx_opmode_streaming)
     vrep.simxSetJointTargetVelocity(clientID, handles[2], right, vrep.simx_opmode_streaming)
     vrep.simxSetJointTargetVelocity(clientID, handles[3], right, vrep.simx_opmode_streaming)
 
 ###
-
-def velocity_mapper(speed, heading):
-    
 
 print ('Program started')
 vrep.simxFinish(-1) # just in case, close all opened connections
@@ -80,8 +76,11 @@ print(errCode)
 err, resolution, image = vrep.simxGetVisionSensorImage(clientID, cam, 0, vrep.simx_opmode_streaming)
 print(err)
 
-listener = Listener(on_press=on_press, on_release=on_release)
-listener.start()
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+#listener = Listener(on_press=on_press, on_release=on_release)
+#listener.start()
 
 data_combined = []
 i = 0
@@ -96,22 +95,25 @@ while True:
     #     cv2.imwrite('savedim.png', cv2.flip(image, 0))
 
     # Lidar data
-    err, ints, floats, strings, outBuffer = vrep.simxCallScriptFunction(clientID, "velodyneVPL_16", 1, "getVelodyneData_function", [], [], [], bytearray(), vrep.simx_opmode_blocking)
-    # index = 0
-    print(len(floats))
-    if len(floats) == 48000:
-        four_floats.append(floats)
-        i += 1
+    # err, ints, floats, strings, outBuffer = vrep.simxCallScriptFunction(clientID, "velodyneVPL_16", 1, "getVelodyneData_function", [], [], [], bytearray(), vrep.simx_opmode_blocking)
+    
+    #print(len(floats))
+    #if len(floats) == 48000:
+    #    four_floats.append(floats)
+    #    i += 1
 
-    if i == 4: #change it to 4 for 360
+    #if i == 4: #change it to 4 for 360
 
-        print("writing")
-        with open('pointcloud.txt', 'w') as write_file:
-            for group in four_floats:
-                for x in group:
-                    write_file.write(str(x) + "\n")
-        print("done writing")
-        break
+    #    print("writing")
+    #    with open('pointcloud.txt', 'w') as write_file:
+    #        for group in four_floats:
+    #            for x in group:
+    #                write_file.write(str(x) + "\n")
+    #    print("done writing")
+    #    break
+    pass
+
+
 
 listener.stop()
 print("Stopped listening. Closing")
